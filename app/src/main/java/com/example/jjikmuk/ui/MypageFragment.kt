1b2c43c438cf857c73bed1d45bd2b51d86afc461
Ojongseok <whdtjr6889@naver.com>
2023-10-07 17:37:52 +0900

    [Feat] Navigation 메뉴 컬러 변경

--- app/src/main/java/com/example/jjikmuk/ui/MypageFragment.kt
+++ app/src/main/java/com/example/jjikmuk/ui/MypageFragment.kt
@@ -5,14 +5,28 @@ import android.view.View
 import com.example.jjikmuk.R
 import com.example.jjikmuk.databinding.FragmentMypageBinding
 import com.example.jjikmuk.util.BaseFragment
+import com.github.mikephil.charting.data.BarEntry
 
 class MypageFragment : BaseFragment<FragmentMypageBinding>(R.layout.fragment_mypage) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupBarChart()
+
         binding.root.setOnClickListener {
 
         }
+    }
+
+    private fun setupBarChart() {
+        val datas = ArrayList<BarEntry>()
+        datas.add(BarEntry(1.2f,20.0f))
+        datas.add(BarEntry(1.2f,20.0f))
+        datas.add(BarEntry(1.2f,20.0f))
+        datas.add(BarEntry(1.2f,20.0f))
+        datas.add(BarEntry(1.2f,20.0f))
+        datas.add(BarEntry(1.2f,20.0f))
+        datas.add(BarEntry(1.2f,20.0f))
     }
 }

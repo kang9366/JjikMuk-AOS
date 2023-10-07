d316a73483ecf9d6959783f829ddaebc8f565721
SeungGu Kang <alex9366@naver.com>
2023-10-07 14:05:57 +0900

    okhttp 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation ("com.squareup.okhttp3:okhttp:4.8.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.8.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.2")
 
 
     implementation("androidx.core:core-ktx:1.9.0")

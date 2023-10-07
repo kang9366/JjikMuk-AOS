d88bc0896a36457fc40805c452452d47b6734202
Ojongseok <whdtjr6889@naver.com>
2023-10-07 16:11:09 +0900

    [Rename] 안쓰는 Fragment 삭제, MypageFragment 추가

--- app/src/main/java/com/example/jjikmuk/ui/MypageFragment.kt
+++ app/src/main/java/com/example/jjikmuk/ui/MypageFragment.kt
@@ -7,8 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.jjikmuk.R
 
-class ThirdFragment : Fragment() {
+class MypageFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_third, container, false)
+        return inflater.inflate(R.layout.fragment_mypage, container, false)
     }
 }
